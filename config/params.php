807c3e5035856bca81830659040643fa0969cb18
luis madrid <luis.madrid@bayardcanada.com>
2019-07-22 13:49:29 -0400

    second commit

--- config/params.php
+++ config/params.php
@@ -3,8 +3,8 @@
 return [
     'adminEmail' => 'admin@example.com',
     'bundleImages' => 'images/bundles/',
-    'api_key' => 'a0c2fcd3a655a9f127e0722591b06dc6',
-    'api_secret' => 'd0c86483a80731ce8ed4162061623a54',
+    'api_key' => '',
+    'api_secret' => '',
     //  List of all the access authorizations needed for the application    
     'access_scopes' => 'read_products, write_products',
     //  List of the shops that are authorized/allowed to install the application
